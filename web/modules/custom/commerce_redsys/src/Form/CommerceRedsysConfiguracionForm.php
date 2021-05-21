7f96557f58840bc1c08e36815f36443bbe1d759c
vidddd <david@hipertintorero.com>
2021-05-21 18:17:59 +0200

    Payment updates

--- web/modules/custom/commerce_redsys/src/Form/CommerceRedsysConfiguracionForm.php
+++ web/modules/custom/commerce_redsys/src/Form/CommerceRedsysConfiguracionForm.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Drupal\commerce_redsys\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
@@ -33,6 +32,11 @@ class CommerceRedsysConfiguracionForm extends ConfigFormBase
             '#type'  => 'fieldset',
             '#title' => $this->t('Configuraciones de La Pasarela de Pago Commerce Redsys'),
         );
+        $form['configuracion']['merchant_url'] = array(
+            '#type'          => 'textfield',
+            '#title'         => 'Merchant Url',
+            '#default_value' => $config->get('merchant_url'),
+        );
         $form['configuracion']['redirect_url'] = array(
             '#type'          => 'textfield',
             '#title'         => 'Redirect Url',
@@ -43,15 +47,35 @@ class CommerceRedsysConfiguracionForm extends ConfigFormBase
             '#title'         => 'Fuc',
             '#default_value' => $config->get('fuc'),
         );
-        $form['configuracion']['terminal'] = array(
-            '#type'          => 'textfield',
-            '#title'         => 'Terminal',
-            '#default_value' => $config->get('terminal'),
-        );
         $form['configuracion']['moneda'] = array(
             '#type'          => 'textfield',
             '#title'         => 'Moneda',
             '#default_value' => $config->get('moneda'),
+        );
+        $form['configuracion']['trans'] = array(
+            '#type'          => 'textfield',
+            '#title'         => 'Trans',
+            '#default_value' => $config->get('trans'),
+        );
+        $form['configuracion']['version'] = array(
+            '#type'          => 'textfield',
+            '#title'         => 'Version',
+            '#default_value' => $config->get('version'),
+        );
+        $form['configuracion']['terminal'] = array(
+            '#type'          => 'textfield',
+            '#title'         => 'Fuc',
+            '#default_value' => $config->get('terminal'),
+        );
+        $form['configuracion']['clave'] = array(
+            '#type'          => 'textfield',
+            '#title'         => 'Clave',
+            '#default_value' => $config->get('clave'),
+        );
+        $form['configuracion']['terminal'] = array(
+            '#type'          => 'textfield',
+            '#title'         => 'Terminal',
+            '#default_value' => $config->get('terminal'),
         );
         return parent::buildForm($form, $form_state);
     }
@@ -62,11 +86,15 @@ class CommerceRedsysConfiguracionForm extends ConfigFormBase
     public function submitForm(array &$form, FormStateInterface $form_state)
     {
         $this->config('commerce_redsys.configuracion')
+            ->set('merchant_url', $form_state->getValue('merchant_url'))
             ->set('redirect_url', $form_state->getValue('redirect_url'))
             ->set('fuc', $form_state->getValue('fuc'))
+            ->set('version', $form_state->getValue('trans'))
+            ->set('version', $form_state->getValue('version'))
+            ->set('clave', $form_state->getValue('clave'))
             ->set('terminal', $form_state->getValue('terminal'))
             ->set('moneda', $form_state->getValue('moneda'))
-            
+            ->set('trans', $form_state->getValue('trans'))
             ->save();
     }
 }
--- web/modules/custom/commerce_redsys/src/PluginForm/RedsysPaymentForm.php
+++ web/modules/custom/commerce_redsys/src/PluginForm/RedsysPaymentForm.php
@@ -1,54 +1,67 @@
 <?php
+
 namespace Drupal\commerce_redsys\PluginForm;
 
 use Drupal\commerce_payment\Exception\PaymentGatewayException;
 use Drupal\commerce_payment\PluginForm\PaymentOffsiteForm as BasePaymentOffsiteForm;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\commerce_redsys\RedsysAPI as RedsysAPI;
+use Drupal\Core\Config\ConfigFactoryInterface;
 
-class RedsysPaymentForm extends BasePaymentOffsiteForm {
+class RedsysPaymentForm extends BasePaymentOffsiteForm
+{
+  /**
+   * The Config Factory.
+   *
+   * @var Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Class constructor.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory)
+  {
+    $this->configFactory = $config_factory;
+  }
 
   /**
    * {@inheritdoc}
    */
-  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state)
+  {
     $form = parent::buildConfigurationForm($form, $form_state);
+    $config = $this->configFactory->get('commerce_redsys.configuracion');
+
     /** @var \Drupal\commerce_payment\Entity\PaymentInterface $payment */
     $payment = $this->entity;
     /** @var \Drupal\commerce_payplug\Plugin\Commerce\PaymentGateway\OffsiteOffsitePayPlug $payment_gateway_plugin */
     $payment_gateway_plugin = $payment->getPaymentGateway()->getPlugin();
     //$payment_gateway_configuration = $payment_gateway_plugin->getConfiguration();
     $red = new RedsysAPI;
-    // Valores de entrada que no hemos cmbiado para ningun ejemplo
-  	$fuc="285964623";
-  	$terminal="003";
-  	$moneda="978";
-  	$trans="0";
-  	$url="";
-  	$urlOK = $form['#return_url'];
+    $urlOK = $form['#return_url'];
     $urlKO = $form['#cancel_url'];
 
-  	//estos dos valores los vamos cambiando en cada ejemplo
-  	$id="0000".$payment->getOrder()->id();//el valor que le damos en cada ejemplo
-  	$amount=$payment->getAmount()->getNumber() * 100;//el valor que le damos en cada ejemplo
+    //estos dos valores los vamos cambiando en cada ejemplo
+    $id = "0000" . $payment->getOrder()->id(); //el valor que le damos en cada ejemplo
+    $amount = $payment->getAmount()->getNumber() * 100; //el valor que le damos en cada ejemplo
 
-    $red->setParameter('DS_MERCHANT_AMOUNT',$amount);
-    $red->setParameter('DS_MERCHANT_ORDER',$id);
-    $red->setParameter('DS_MERCHANT_MERCHANTCODE',$fuc);
-    $red->setParameter('DS_MERCHANT_CURRENCY',$moneda);
-    $red->setParameter('DS_MERCHANT_TRANSACTIONTYPE',$trans);
-    $red->setParameter('DS_MERCHANT_TERMINAL',$terminal);
-    $red->setParameter('DS_MERCHANT_MERCHANTURL',$url);
+    $red->setParameter('DS_MERCHANT_AMOUNT', $amount);
+    $red->setParameter('DS_MERCHANT_ORDER', $id);
+    $red->setParameter('DS_MERCHANT_MERCHANTCODE', $config->get('fuc'));
+    $red->setParameter('DS_MERCHANT_CURRENCY', $config->get('moneda'));
+    $red->setParameter('DS_MERCHANT_TRANSACTIONTYPE', $config->get('trans'));
+    $red->setParameter('DS_MERCHANT_TERMINAL', $config->get('terminal'));
+    $red->setParameter('DS_MERCHANT_MERCHANTURL', $config->get('merchant_url'));
     $red->setParameter('DS_MERCHANT_URLOK', $urlOK);
     $red->setParameter('DS_MERCHANT_URLKO', $urlKO);
 
-    $version="HMAC_SHA256_V1";
     $params = $red->createMerchantParameters();
-    $clave = 'he5YgpkWQYKw0byM0j+dvdVhbN4st75q';
+    $clave = '';
 
     $signature = $red->createMerchantSignature($clave);
     $data = [
-      'Ds_SignatureVersion' => $version,
+      'Ds_SignatureVersion' => $config->get('version'),
       'Ds_MerchantParameters' => $params,
       'Ds_Signature' => $signature
     ];
@@ -58,9 +71,14 @@ class RedsysPaymentForm extends BasePaymentOffsiteForm {
         $form[$name] = ['#type' => 'hidden', '#value' => $value];
       }
     }
-
-     $redirect_url = 'https://sis.redsys.es/sis/realizarPago';
     return $this->buildRedirectForm($form, $form_state, $redirect_url, $data, self::REDIRECT_POST);
   }
 
+  /**
+   * {@inheritdoc}.
+   */
+  public function getFormId()
+  {
+    return 'redsys_payment_form';
+  }
 }
